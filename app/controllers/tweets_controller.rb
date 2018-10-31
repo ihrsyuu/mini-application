d4bb79005b231280d89296380f312e019f009fe7
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-10-31 13:28:05 +0900

    いろいろ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,7 +12,7 @@ def new
 end
 
 def create
-  Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  Tweet.create(text: tweet_params[:text], user_id: current_user.id)
   redirect_to :action => "index"
 end
 
@@ -40,4 +40,6 @@ end
 
 def redirect_to_index
 redirect_to :action => "index" unless user_signed_in?
+end
+
 end
